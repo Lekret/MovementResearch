c2c1656de88677e672032099e62c38ad53a87e4b
Lekret <lekret399@mail.ru>
2022-11-23 12:52:24 +0300

    AddMomentum

---
+++ Assets/_Prototyping/MovementFundamentals/AddMomentum.cs
@@ -0,0 +1,26 @@
+﻿using CMF;
+using UnityEngine;
+
+namespace _Prototyping.MovementFundamentals
+{
+    public class AddMomentum : MonoBehaviour
+    {
+        public AdvancedWalkerController WalkerController;
+        public Vector3 Momentum;
+        
+        private bool _pressed;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.J))
+                _pressed = true;
+        }
+
+        private void FixedUpdate()
+        {
+            if (_pressed && WalkerController)
+                WalkerController.AddMomentum(Momentum);
+            _pressed = false;
+        }
+    }
+}
